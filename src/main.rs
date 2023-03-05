f454a0091f7495e3ffaa352ee5b8254c7faefe9b
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-05 23:13:44 +0000

    read words from file and select random line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+
+    let wordle_words = fs::read_to_string("wordle-words.txt")
+        .expect("Unable to read file");
+
+    // random line (12971 lines in file)
+    let word_line = rand::thread_rng().gen_range(0..12971);
+
+    let wordle = wordle_words.lines().nth(word_line).unwrap();
+
+    println!("the word to guess is {}", wordle); // TODO: remove this line
 }
