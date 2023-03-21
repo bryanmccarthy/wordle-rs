a108cb2ad753e5eededce9e5a95000a08b3f0ccc
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-21 21:27:32 +0000

    get user guess fn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
+use std::io::{BufReader, BufRead};
 use std::fs;
+use std::fs::File;
 use rand::Rng;
 use colored::*;
 
@@ -18,11 +20,10 @@ fn main() {
     game(wordle);
 }
 
-fn game(wordle: &str) {
-    let mut guess_count = 1;
-    println!("Wordle!");
+fn get_user_guess() -> String {
     loop {
         let mut guess = String::new();
+        let wordle_words_file_path = "wordle-words.txt";
 
         println!("Enter your guess: ");
 
@@ -30,13 +31,46 @@ fn game(wordle: &str) {
         stdin.read_line(&mut guess)
             .expect("Failed to read line");
 
-        if guess.len() != 6 { // TODO: why 6? 
+        if guess.len() != 6 {
             println!("Please enter a 5 letter word");
             continue;
         }
 
-        // TODO: ensure the word is a word in the dictionary
+        match find_word_in_file(wordle_words_file_path, &guess) {
+            Ok(found) => {
+                if found {
+                    return guess;
+                } else {
+                    println!("That is not a valid word.");
+                    continue;
+                }
+            }
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+}
 
+fn find_word_in_file(file_path: &str, word_to_find: &str) -> io::Result<bool> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.trim() == word_to_find.trim() {
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
+}
+
+fn game(wordle: &str) {
+    let mut guess_count = 1;
+    println!("Wordle!");
+    loop {
+        let guess = get_user_guess();
+
+        // TODO: remove
         for (i, letter) in wordle.chars().enumerate() {
             println!("{} {}", i, letter);
         }
